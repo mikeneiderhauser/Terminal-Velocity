6e0977ad10a50a7e5599cb0cbd0bd0c0fef8642c
Matthew Schechtman <matthew.d.schechtman@gmail.com>
2012-11-09 08:38:36 -0500

    Created solution projets
    Created a new project for each module of the solution

---
+++ Terminal-Velocity/Interfaces/ITesting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interfaces
+{
+    interface ITesting
+    {
+        bool DoTest(out int pass, out int fail, out string[] messages);
+    }
+}
