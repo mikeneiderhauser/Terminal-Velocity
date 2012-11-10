176b52e9b86ed534df3a84bc0aea80e79fae294c
Matthew Schechtman <matthew.d.schechtman@gmail.com>
2012-11-10 12:45:28 -0500

    Added Tester, changed signature of ITesting

--- Terminal-Velocity/Interfaces/ITesting.cs
+++ Terminal-Velocity/Interfaces/ITesting.cs
@@ -7,6 +7,6 @@ namespace Interfaces
 {
     interface ITesting
     {
-        bool DoTest(out int pass, out int fail, out string[] messages);
+        bool DoTest(out int pass, out int fail, out List<string> messages);
     }
 }
---
+++ Terminal-Velocity/Testing/Tester.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Interfaces;
+
+namespace Testing
+{
+    public class Tester
+    {
+            static int Main(String[] args)
+            {
+                int pass = 0;
+                int fail = 0;
+
+                var type = typeof(ITesting);
+                var types = AppDomain.CurrentDomain.GetAssemblies().ToList().SelectMany(s => s.GetTypes()).Where(p => type.IsAssignableFrom(p));
+                foreach (Type t in types)
+                {
+                    if (!t.IsInterface)
+                    {
+                        object o = Activator.CreateInstance(t);
+                        MethodInfo info = t.GetMethod("DoTest");
+                        Boolean result = (Boolean) info.Invoke(o, new object[] { });
+
+                        if (result)
+                        {
+                            pass++;
+                            Console.WriteLine(string.Format("[Test] success: {0}", t.ToString()));
+                        }
+                        else
+                        {
+                            fail++;
+                            Console.WriteLine(string.Format("[Test] FAILED: {0}", t.ToString()));
+                        }
+                    }
+                }
+
+                Console.WriteLine("\n\n");
+                Console.WriteLine(string.Format("All tests completed: {0} out of {1} tests passed!", pass, pass + fail));
+                Console.WriteLine("=========================================================\nPress enter to continue...");
+                Console.ReadLine();
+
+                return 0;
+            }
+        }
+    }
+}
