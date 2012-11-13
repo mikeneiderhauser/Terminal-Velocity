67532adf849789fd93066097df499a20175c5938
Derek <dcf11@pitt.edu>
2012-11-13 17:44:53 -0500

    Added interface and started implemenation

--- Terminal-Velocity/Interfaces/ITrainModel.cs
+++ Terminal-Velocity/Interfaces/ITrainModel.cs
@@ -5,7 +5,26 @@ using System.Text;
 
 namespace Interfaces
 {
-    interface ITrainModel
+    public interface ITrainModel
     {
+        public int TrainID { get; }
+        public int TotalLength { get; }
+        public double TotalMass { get; }
+        public string InformationLog { get; }
+        public bool LightsOn { get; set; }
+        public bool DoorOpen { get; set; }
+        public int Temperature { get; set; }
+        public double CurrentAcceleration { get; }
+        public double CurrentVelocity { get; }
+        public double CurrentPosition { get; }
+        public int MaxCapacity { get; }
+        public int NumPassengers { get; set; }
+        public int NumCrew { get; set; }
+        public bool BrakeFailure { get; }
+        public bool EngineFailure { get; }
+        public bool SignalPickupFailure { get; }
+
+        public bool ChangeMovement(double power);
+        public void EmergencyBrake();
     }
 }
