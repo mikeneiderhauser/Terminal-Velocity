54b76740cad6d242c4e07b1d7395a0869b084851
Mike Neiderhauser <mikeneiderhauser@gmail.com>
2012-11-14 19:17:58 -0500

    Try protection fix

--- Terminal-Velocity/CTCOffice/CTCOfficeGUI.cs
+++ Terminal-Velocity/CTCOffice/CTCOfficeGUI.cs
@@ -23,6 +23,7 @@ namespace CTCOffice
             _ctcOffice = ctc;
             _environment = env;
 
+            //commit comment
             //subscribe to Environment Tick
             _environment.Tick += new EventHandler<TickEventArgs>(_environment_Tick);
         }
