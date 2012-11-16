fb78da8189dbaa4b040aca9233c7b8e39bd3fe03
Matthew Schechtman <matthew.d.schechtman@gmail.com>
2012-11-16 09:50:04 -0500

    TrainModel->prototype
    This merge attempted to delete ITrain, which was not allowed

--- Terminal-Velocity/Interfaces/ITrainModel.cs
+++ Terminal-Velocity/Interfaces/ITrainModel.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace Interfaces
 {
-    interface ITrainModel
+    public interface ITrainModel
     {
+        int TrainID { get; }
+        int Length { get; }
+        double TotalMass { get; }
+        string InformationLog { get; }
+        bool LightsOn { get; set; }
+        bool DoorsOpen { get; set; }
+        int Temperature { get; set; }
+        double CurrentAcceleration { get; }
+        double CurrentVelocity { get; }
+        double CurrentPosition { get; }
+        int MaxCapacity { get; }
+        int NumPassengers { get; set; }
+        int NumCrew { get; set; }
+        bool BrakeFailure { get; }
+        bool EngineFailure { get; }
+        bool SignalPickupFailure { get; }
+        IBlock CurrentBlock { get; }
+
+        bool ChangeMovement(double power);
+        void EmergencyBrake();
     }
 }
