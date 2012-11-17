491de57b6b57e14058f37bf3d46075772bc93617
Mike Neiderhauser <mikeneiderhauser@gmail.com>
2012-11-16 21:32:06 -0500

    Attempt at testing CTC Office GUI through Tester

--- Terminal-Velocity/Testing/Tester.cs
+++ Terminal-Velocity/Testing/Tester.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.Windows.Forms;
 
 using Interfaces;
 
@@ -12,58 +13,70 @@ namespace Testing
     {
         static int Main(String[] args)
         {
-            int totalPass = 0;
-            int totalFail = 0;
-            int totalFatal = 0;
-
-            var type = typeof(ITesting);
-            var types = AppDomain.CurrentDomain.GetAssemblies().ToList().SelectMany(s => s.GetTypes()).Where(p => type.IsAssignableFrom(p));
-            foreach (Type t in types)
+            int x = 0;
+            if(args[0].Equals("CTCTester"))
             {
-                if (!t.IsInterface)
-                {
-                    Console.WriteLine("==================================================");
-                    Console.WriteLine(string.Format("[{0}]", t.ToString()));
-
-                    object o = Activator.CreateInstance(t);
-                    MethodInfo info = t.GetMethod("DoTest");
-                    object[] parameters = new object[] { null, null, null };
-                    Boolean result = (Boolean)info.Invoke(o, parameters);
-
-                    if (result)
-                    {
-                        int pass = (int) parameters[0];
-                        int fail = (int) parameters[1];
-                        List<string> messages = (List<string>) parameters[2];
-
-                        Console.WriteLine(string.Format("{0} tests passed", pass));
-                        Console.WriteLine(string.Format("{0} non-fatal errors", fail));
-                        foreach (string s in messages)
-                            Console.WriteLine(string.Format("Message : {0}", s));
-
-                        totalPass += pass;
-                        totalFail += fail;
-                    }
-                    else
-                    {
-                        Console.WriteLine(string.Format("A fatal error has occured"));
-                        totalFail++;
-                    }
-
-                    Console.WriteLine("\n");
-                }
+                Form CTCTestWindow = new Form();
+                TerminalVelocity.Environment env = new TerminalVelocity.Environment();
+                CTCOffice.CTCOffice ctc = new CTCOffice.CTCOffice(env, env.PrimaryTrackControllerRed, env.PrimaryTrackControllerGreen);
+                CTCTestWindow.Controls.Add(new CTCOffice.CTCOfficeGUI(env,ctc));
             }
+            else
+            {
+                int totalPass = 0;
+                int totalFail = 0;
+                int totalFatal = 0;
 
-            Console.WriteLine("==================================================");
-            Console.WriteLine("==================================================");
-            Console.WriteLine("Testing completed.\n");
-            Console.WriteLine(string.Format("Total Passed : {0} \nTotal Fail : {1} \nTotal Fatal Errors : {2}", totalPass, totalFail, totalFatal));
-            Console.WriteLine("==================================================");
-            Console.WriteLine("==================================================");
-            Console.WriteLine("Press enter to continue...");
-            Console.ReadLine();
+                var type = typeof(ITesting);
+                var types = AppDomain.CurrentDomain.GetAssemblies().ToList().SelectMany(s => s.GetTypes()).Where(p => type.IsAssignableFrom(p));
+                foreach (Type t in types)
+                {
+                    if (!t.IsInterface)
+                    {
+                        Console.WriteLine("==================================================");
+                        Console.WriteLine(string.Format("[{0}]", t.ToString()));
 
+                        object o = Activator.CreateInstance(t);
+                        MethodInfo info = t.GetMethod("DoTest");
+                        object[] parameters = new object[] { null, null, null };
+                        Boolean result = (Boolean)info.Invoke(o, parameters);
+
+                        if (result)
+                        {
+                            int pass = (int) parameters[0];
+                            int fail = (int) parameters[1];
+                            List<string> messages = (List<string>) parameters[2];
+
+                            Console.WriteLine(string.Format("{0} tests passed", pass));
+                            Console.WriteLine(string.Format("{0} non-fatal errors", fail));
+                            foreach (string s in messages)
+                                Console.WriteLine(string.Format("Message : {0}", s));
+
+                            totalPass += pass;
+                            totalFail += fail;
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("A fatal error has occured"));
+                            totalFail++;
+                        }
+
+                        Console.WriteLine("\n");
+                    }
+                }
+
+                Console.WriteLine("==================================================");
+                Console.WriteLine("==================================================");
+                Console.WriteLine("Testing completed.\n");
+                Console.WriteLine(string.Format("Total Passed : {0} \nTotal Fail : {1} \nTotal Fatal Errors : {2}", totalPass, totalFail, totalFatal));
+                Console.WriteLine("==================================================");
+                Console.WriteLine("==================================================");
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+
+                
+            }//end connand line switch
             return 0;
-        }
+        }//end main
     }
 }
