a3b06ee5a8e61ceea854977c6d8e114acb188152
Jason Gerlowski <jeg90@pitt.edu>
2012-11-18 15:50:09 -0500

    DBCreator.cs public method definition

---
+++ Terminal-Velocity/TrackModel/DBCreator.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interfaces;
+using Utility;
+
+namespace TrackModel
+{
+
+    public class DBCreator
+    {
+        //Private parameters
+	private int _blockID;
+
+        public DBCreator(int bID)
+        {
+		_blockID=bID;
+        }
+		
+		
+	//Public methods
+	public int parseInputFile(string fPath)
+	{
+
+	}
+
+	public int handleUpdateRequest(int bID, int newBlockSize)
+	{
+
+	}
+
+	//Properties
+    }
+}
