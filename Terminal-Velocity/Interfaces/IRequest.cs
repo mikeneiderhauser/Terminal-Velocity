3ad7ccda3018173da35fb3b9ceb40f7ca5a28084
Mike Neiderhauser <mikeneiderhauser@gmail.com>
2012-11-19 16:22:15 -0500

    Fixed typo in IRequest. Merge CTC into Proto

--- Terminal-Velocity/Interfaces/IRequest.cs
+++ Terminal-Velocity/Interfaces/IRequest.cs
@@ -24,6 +24,7 @@ namespace Interfaces
 
         /// <summary>
         /// Property to access _trainSpeed
+        /// Fixed interface issue
         /// </summary>
         double TrainSpeed { get; }
 
