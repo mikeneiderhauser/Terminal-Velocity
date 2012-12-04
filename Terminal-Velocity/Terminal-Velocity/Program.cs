3e54de78e867b580f848a9f7f12b7126a6fff9a3
unknown <coelabs@meas-embed04.engr.nt.pitt.edu>
2012-12-04 15:34:07 -0500

    Added definition of Program.cs  so that it launches the application

--- Terminal-Velocity/Terminal-Velocity/Program.cs
+++ Terminal-Velocity/Terminal-Velocity/Program.cs
@@ -1,6 +1,15 @@
 ﻿using System;
 using System.Windows.Forms;
 
+using SimulationEnvironment;
+using CTCOffice;
+using SystemScheduler;
+using TrackController;
+using TrackModel;
+using TrainController;
+using TrainModel;
+
+
 namespace TerminalVelocity
 {
     public class Program
@@ -8,9 +17,29 @@ namespace TerminalVelocity
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Environment
+            SimulationEnvironment.SimulationEnvironment e = new SimulationEnvironment.SimulationEnvironment();
+            
+            // TrackModel
+            TrackModel.TrackModel trackModel = new TrackModel.TrackModel(e);
+            TrackModel.TrackModelGUI trackModelGui = new TrackModelGUI(e, trackModel);
+
+            // CTCOffice
+            CTCOffice.CTCOffice ctcOffice = new CTCOffice.CTCOffice(e, e.PrimaryTrackControllerRed, e.PrimaryTrackControllerGreen);
+            CTCOffice.CTCOfficeGUI ctcOfficeGui = new CTCOfficeGUI(e, ctcOffice);
+
+            // Scheduler
+            SystemScheduler.SystemScheduler scheduler = new SystemScheduler.SystemScheduler(e, ctcOffice);
+            SystemScheduler.SystemSchedulerGUI schedulerGui = new SystemScheduler.SystemSchedulerGUI(e, scheduler, ctcOffice);
+
+            Form ctcForm = new Form() { Controls = { ctcOfficeGui }, AutoSize = true };
+            Form schedulerForm = new Form() { Controls = { schedulerGui }, TopLevel = false, Parent = ctcForm };
+            Form trackModelForm = new Form() { Controls = { trackModelGui }, TopLevel = false, Parent = ctcForm };
+
+            Application.Run(ctcForm);
         }
     }
 }
